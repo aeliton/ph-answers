d5ca8ee46c278102226025188d024b04466bc744
Aeliton G. Silva <aeliton@bugtec.com.br>
2020-05-11 21:42:05 -0300

    Add 6.8
    Also fix merge function

--- ph2nd.hs
+++ ph2nd.hs
@@ -1,13 +1,31 @@
 import Data.Char
 --import Prelude hiding ((^))
+-- 6.8
+halves :: [a] -> ([a], [a])
+halves xs = (take n xs, drop n xs)
+    where n = div (length xs) 2
 
--- 7
 merge :: Ord a => [a] -> [a] -> [a]
 merge [] ys = ys
 merge xs [] = xs
 merge (x:xs) (y:ys)
+    | x < y = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort as) (msort bs)
+    where (as, bs) = halves xs
+
+
+-- 6.7
+{-merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
     | x < y = x : y : merge xs ys
-    | otherwise = y : x : merge xs ys
+    | otherwise = y : x : merge xs ys-}
 
 -- 6.6 e.
 elem' :: Eq a => a -> [a] -> Bool
